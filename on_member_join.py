e3ec70eb7e98a94ba57be6645e35b8552fd15f4c
CedricThode <cedricat@stud.ntnu.no>
2023-03-28 01:45:59 +0200

    Update on_member_join.py

--- on_member_join.py
+++ on_member_join.py
@@ -11,7 +11,7 @@ async def on_member_join(member):
     invites = await member.guild.invites()
     # Check if the invite is the specific link
     for invite in invites:
-        if "https://discord.gg/Qhvv92HYSt" in invite.url:
+        if "https://discord.gg/26Sjm6Qett" in invite.url:
             # Gives role "=CALUM= Private" 
             role = discord.utils.get(member.guild.roles, name="=CALUM= Private")
             await member.add_roles(role)
